7d9b5441247a61db944cef4c268b16eaf5c6b8b7
Ritwik Sharma <ritwik.1204@gmail.com>
2018-06-14 12:55:49 +0530

    Update LogExtraction.py

--- LogExtraction.py
+++ LogExtraction.py
@@ -5,18 +5,23 @@ id = set([])
 views = ""
 count = 0
 lines = tuple(open("production.log", 'r'))
-#f =open("b.log","w") 
+#f =open("output.log","w") 
 for i in range(1, len(lines)):
 	line = lines[i]
 	if line.find("Views")!=-1:
 		id = line[20:28]
 		count = count +1
 		time = line[12:19]
-	   	totaltime = line[line.find("in") + 2: line.find("in") + 8]
-		print "Time", time
-		print "Totaltime", totaltime
-		print "ID", id
+	   	totaltime = line[line.find("in")+2: line.find("in")+7]
+	   	Views = line[line.find("Views")+6: line.find("Views")+13]
+	   	ActiveRecord = line[line.find("ActiveRecord")+14: line.find("ActiveRecord")+19]
+		print "Time :", time
+		print "Totaltime :", totaltime
+		print "ID :", id
+		print "Views :", Views
+		print "ActiveRecord :", ActiveRecord
 		print "-------------------------------------------------"	
+	#   Print to File
 	#	print >> f, time
 	#	print >> f, totaltime
 	#	print >> f, id
