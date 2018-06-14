757b05a3cf722a1b17c6c96357eeb304bafb32b5
Ritwik Sharma <ritwik.1204@gmail.com>
2018-06-14 15:56:46 +0530

    Add Logs_acctoTime.py

--- Logs_acctoTime.py
+++ Logs_acctoTime.py
@@ -6,7 +6,4 @@ print contents.index("04:00:24")
 start = contents.index("04:00:16") + 8 
 end = contents.index("04:00:24")
 str = contents[start:end] 
-print str
-
-
-                   
+print str            
