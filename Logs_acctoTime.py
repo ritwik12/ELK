23c50e7a1117f752b24ced61368f8556bde60746
Ritwik Sharma <ritwik.1204@gmail.com>
2018-06-14 15:56:12 +0530

    Add Logs_acctoTime.py

---
+++ Logs_acctoTime.py
@@ -0,0 +1,12 @@
+file = open("production.log", "rt") 
+contents = file.read()         
+file.close()     
+print contents.index("04:00:16")
+print contents.index("04:00:24")   
+start = contents.index("04:00:16") + 8 
+end = contents.index("04:00:24")
+str = contents[start:end] 
+print str
+
+
+                   
